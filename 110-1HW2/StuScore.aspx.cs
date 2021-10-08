7c9b504895c41229d26c462f7cb93a11f1f46cfc
偉國 曾 <108111113@mail.aeust.edu.tw>
2021-10-08 10:37:40 +0800

    class Score

---
+++ 110-1HW2/StuScore.aspx.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace _110_1HW2
+{
+    public partial class StuScore : System.Web.UI.Page
+    {
+        public class Scores
+        {
+            public string s_Id;
+            public float f_ChiS;
+            public float f_MathS;
+            public float f_EngS;
+        }
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            Scores o_Stu = new Scores();
+            o_Stu.s_Id = "A123456789";
+            o_Stu.f_ChiS = 62f;
+            o_Stu.f_MathS = 73f;
+            o_Stu.f_EngS = 84f;
+
+            Response.Write("s_Id = "+ o_Stu.s_Id + 
+                           ", f_ChiS = "+ o_Stu.f_ChiS.ToString() + 
+                           ", f_MathS = " + o_Stu.f_MathS.ToString() + 
+                           ", f_EngS = " + o_Stu.f_EngS.ToString());
+        }
+    }
+}
